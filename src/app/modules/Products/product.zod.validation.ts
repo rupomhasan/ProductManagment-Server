1e99abe5484c96b440493ca09c861f3f80765926
rupomhasan <rupom.hasan607299@gmail.com>
2024-05-23 15:46:14 +0600

    Add TypesScript types , Mongooes schema , and Zod validation for product type

---
+++ src/app/modules/Products/product.zod.validation.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+export const variantsValidationSchema = z.object({
+  type: z.string(),
+  value: z.string(),
+});
+
+export const inventoryValidationSchema = z.object({
+  quantity: z.number(),
+  inStock: z.boolean(),
+});
+
+export const productValidationSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  tags: z.array(z.string()),
+  variants: z.array(variantsValidationSchema),
+  inventory: inventoryValidationSchema,
+});
